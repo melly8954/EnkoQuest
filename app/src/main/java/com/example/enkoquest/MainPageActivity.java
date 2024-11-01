26271df89e2d2dd311358165737852a39db1c106
KIMSMING <alstjdrla78@gmail.com>
2024-11-01 14:31:03 +0900

    Layout
    Layout

--- app/src/main/java/com/example/enkoquest/MainPageActivity.java
+++ app/src/main/java/com/example/enkoquest/MainPageActivity.java
@@ -1,6 +1,9 @@
 package com.example.enkoquest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainPageActivity extends AppCompatActivity {
+public class MainPageActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn01, btn02, btn03;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,29 @@ public class MainPageActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        btn01 = findViewById(R.id.buttonMyPage);
+        btn02 = findViewById(R.id.buttonChallengeMode);
+        btn03 = findViewById(R.id.buttonStoryMode);
+
+        btn01.setOnClickListener(this);
+        btn02.setOnClickListener(this);
+        btn03.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.buttonMyPage) {
+            Intent intent = new Intent(MainPageActivity.this, MyPageActivity.class);
+            startActivity(intent);
+        }
+        if (view.getId() == R.id.buttonChallengeMode) {
+            Intent intent = new Intent(MainPageActivity.this, ChallengeActivity.class);
+            startActivity(intent);
+        }
+        if (view.getId() == R.id.buttonStoryMode) {
+            Intent intent = new Intent(MainPageActivity.this, StoryActivity.class);
+            startActivity(intent);
+        }
     }
 }
