2ebc4dd1b9a663622799ed21366fc133fd72919f
haily <49448330+melly8954@users.noreply.github.com>
2024-11-02 21:04:39 +0900

    Update MyPageActivity.java

--- app/src/main/java/com/example/enkoquest/MyPageActivity.java
+++ app/src/main/java/com/example/enkoquest/MyPageActivity.java
@@ -16,6 +16,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.enkoquest.user.UserAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
